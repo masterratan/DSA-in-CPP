d4776e93099b105977f1f67c12a317bca700bba5
masterratan <itsratan.choudhary@gmail.com>
2026-01-16 10:33:15 +0530

    added graphs

---
+++ C9 Graphs/dfs.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph {
+  int V;
+  list<int> *l; // dynamic array => int *arr
+
+public:
+  Graph(int V){
+    this->V = V;
+    l = new list<int> [V];
+  }
+
+  void addEdge(int u, int v){
+    l[u].push_back(v);
+    l[v].push_back(u);
+  }
+
+  // BFS Traversal
+  void bfs(){
+    queue<int> q;
+    vector<bool> vis(V, false);
+
+    q.push(0);
+    vis[0] = true;
+
+    while(q.size() > 0){
+      int u = q.front();
+      q.pop();
+
+      cout<<u<<" ";
+      
+      for(int v : l[u]){
+        if(!vis[v]) {
+          vis[v] = true; // Imp: Elements are marked visited only when they are about to get pushed in the queue
+          q.push(v);
+        }
+      }
+    }
+
+    cout<<endl;
+  }
+
+  void dfsHelper(int u, vector<bool> &vis){ // O(V+E)
+    cout<<u<<" ";
+    vis[u] = true;
+    for(int v : l[u]){
+      if(!vis[v]) dfsHelper(v, vis);
+    }
+  }
+  void dfs(){
+    vector<bool> vis(V, false);
+    for(int i=0; i<V; i++){ // for disconnected graph
+      if(!vis[i]){
+        dfsHelper(i, vis);
+      }
+    }
+    cout<<endl;
+  }
+};
+
+int main(){
+  Graph g(5);
+
+  g.addEdge(0, 1);
+  g.addEdge(1, 2);
+  g.addEdge(1, 3);
+  g.addEdge(2, 4);
+
+  g.dfs();
+
+  return 0;
+}
